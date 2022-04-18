a06d8ccd35e9ab88fd1dae8fd25bf22968f45544
ArsenUtepbergenov <arsen.utepbergenov@yandex.ru>
2022-04-18 12:28:35 +0300

    master/init react, redux-toolkit

---
+++ src/features/users/AddUser.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import TextField from '../../components/TextField'
+import Button from '../../components/Button'
+import { useDispatch } from 'react-redux'
+import { addUser } from './userSlice'
+import { v4 as uuidv4 } from 'uuid'
+
+const AddUser = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [values, setValues] = useState({
+    name: '',
+    email: ''
+  })
+
+  const handleNameChange = (e) => {
+    setValues({ ...values, name: e.target.value })
+  }
+
+  const handleEmailChange = (e) => {
+    setValues({ ...values, email: e.target.value })
+  }
+
+  const handleAddUser = () => {
+    setValues({ name: '', email: '' })
+    dispatch(addUser({
+      id: uuidv4(),
+      name: values.name,
+      email: values.email
+    }))
+    navigate('/')
+  }
+
+  return (
+    <div className="mt-10 max-w-xl mx-auto">
+      <TextField
+        label="Name"
+        value={values.name}
+        inputProps={{ type: 'text', placeholder: 'John' }}
+        onChange={handleNameChange}
+      />
+      <br />
+      <TextField
+        label="Email"
+        value={values.email}
+        inputProps={{ type: 'email', placeholder: 'john@mail.com' }}
+        onChange={handleEmailChange}
+      />
+      <Button onClick={handleAddUser}>Submit</Button>
+    </div>
+  )
+}
+
+export default AddUser
---
+++ src/features/users/EditUser.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import TextField from '../../components/TextField'
+import Button from '../../components/Button'
+import { useSelector, useDispatch } from "react-redux"
+import { editUser } from './userSlice'
+
+const EditUser = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const params = useParams()
+  const users = useSelector(store => store.users)
+  console.log(users);
+  console.log(params);
+  const existingUser = users.filter(user => user.id === params.id)
+  const { name, email } = existingUser[0]
+
+  const [values, setValues] = useState({
+    name,
+    email
+  })
+
+  const handleNameChange = (e) => {
+    setValues({ ...values, name: e.target.value })
+  }
+
+  const handleEmailChange = (e) => {
+    setValues({ ...values, email: e.target.value })
+  }
+
+  const handleEditUser = () => {
+    setValues({ name: '', email: '' })
+    dispatch(editUser({
+      id: params.id,
+      name: values.name,
+      email: values.email
+    }))
+    navigate('/')
+  }
+
+  return (
+    <div className="mt-10 max-w-xl mx-auto">
+      <TextField
+        label="Name"
+        value={values.name}
+        inputProps={{ type: 'text', placeholder: 'John' }}
+        onChange={handleNameChange}
+      />
+      <br />
+      <TextField
+        label="Email"
+        value={values.email}
+        inputProps={{ type: 'email', placeholder: 'john@mail.com' }}
+        onChange={handleEmailChange}
+      />
+      <Button onClick={handleEditUser}>Edit</Button>
+    </div>
+  )
+}
+
+export default EditUser
