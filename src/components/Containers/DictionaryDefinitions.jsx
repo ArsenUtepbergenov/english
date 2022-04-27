9a23ee4300c9a96e8c8a045e6b483e9a5f70952e
ArsenUtepbergenov <arsen.utepbergenov@yandex.ru>
2022-04-27 15:39:56 +0300

    optimize/updated dictionary definitions

--- src/components/Containers/DictionaryDefinitions.jsx
+++ src/components/Containers/DictionaryDefinitions.jsx
@@ -1,5 +1,6 @@
 import { Masonry } from "@mui/lab"
 import { Box, capitalize, Typography } from "@mui/material"
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered'
 import SimpleList from "components/Lists/SimpleList"
 import { DefinitionItemColor, PartOfSpeech } from 'models/dictionary'
 
@@ -10,16 +11,23 @@ function DictionaryDefinitions({ partsOfSpeech }) {
 
   const getItem = (partOfSpeech) => {
     const items = partsOfSpeech[partOfSpeech]
+    const length = items?.length
 
     return (
-      <Box sx={{ p: 1, maxHeight: 360, overflowY: 'auto', bgcolor: getBgColor(partOfSpeech) }}>
-        <Typography variant="h6" align="center">{capitalize(`${partOfSpeech}s`)}</Typography>
+      <>
         {
-          items?.length ?
-            <SimpleList items={items} prop="definition" /> :
-            <Box component="span" sx={{ pl: '1.4em' }}>No Definitions</Box>
+          length ?
+            <Box sx={{ p: 1, maxHeight: 360, overflowY: 'auto', bgcolor: getBgColor(partOfSpeech) }}>
+              <Typography variant="h6" align="center">{capitalize(`${partOfSpeech}s`)}</Typography>
+              {
+                length ?
+                  <SimpleList items={items} prop="definition" /> :
+                  <Box sx={{ pl: '1.3em' }}><FormatListNumberedIcon /></Box>
+              }
+            </Box> :
+            null
         }
-      </Box>
+      </>
     )
   }
 
